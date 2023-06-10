5b4d4eb2ab90492d900ea0eb241d65c0f4b911c2
jhigger <njohnjhigger@gmail.com>
2023-06-11 04:27:49 +0800

    change font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,8 @@
 import { type AppType } from "next/dist/shared/lib/utils";
 import "~/styles/globals.css";
-import { Poppins } from "next/font/google";
+import { Noto_Sans_TC } from "next/font/google";
 
-const font = Poppins({
+const font = Noto_Sans_TC({
 	subsets: ["latin"],
 	weight: ["400", "700"],
 });
