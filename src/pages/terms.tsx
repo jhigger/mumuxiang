26612d92ca22caf16bd17051101da16df9da0f2b
jhigger <njohnjhigger@gmail.com>
2023-06-26 20:08:17 +0800

    edit head

--- src/pages/terms.tsx
+++ src/pages/terms.tsx
@@ -9,13 +9,10 @@ const Terms = () => {
 		<>
 			<Head>
 				<title>木目用户协议</title>
-				<meta
-					name="description"
-					content="Artisse AI - Changing photography forever"
-				/>
-				<link rel="canonical" href="https://artisse.ai/" />
+				<meta name="description" content="木目，你的美，刮目“相”看" />
 				<meta name="author" content="https://github.com/jhigger" />
-				<link rel="icon" href="/logo.webp" />
+				<link rel="canonical" href="https://mumuxiang.com/" />
+				<link rel="icon" href="/artisse favicon.png" />
 			</Head>
 			<div className="container mx-auto my-32 w-max">
 				<ReactMarkdown
